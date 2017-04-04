5fb24220ca189bf17fab2b9c09ba82aaea2fa7b7
Bakhtiyor Khodjayev <bakhtiyor.k@gmail.com>
2017-04-04 10:20:31 +0500

    Cleanup

--- app/src/main/java/com/example/ridemetricsdksample/CustomApplication.java
+++ app/src/main/java/com/example/ridemetricsdksample/CustomApplication.java
@@ -3,22 +3,15 @@ package com.example.ridemetricsdksample;
 import android.app.Application;
 import android.content.Intent;
 
-import com.ridemetric.sdk.RideMetricSDKconfig;
-
 public class CustomApplication extends Application {
     //rideMetricSdkKey should be retrieved from ridemetric licensing site
 
     //license enabling start stop of the trip and disabling auto detection
     static final String rideMetricSdkKey = "YOUR-RIDEMETRIC-SDK-KEY";
-    static boolean USE_SERVICE = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        if (USE_SERVICE) {
-            startService(new Intent(this, SampleService.class));
-        } else {
-            RideMetricSDKconfig.initApp(this, rideMetricSdkKey);
-        }
+        startService(new Intent(this, SampleService.class));
     }
 }
