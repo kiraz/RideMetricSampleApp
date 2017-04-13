50370ca799de37f16c66704058d183937b2bfbdd
Bakhtiyor Khodjayev <bakhtiyor.k@gmail.com>
2017-04-13 20:22:57 +0500

    Update to SDK v.4.1.43

--- app/src/main/java/com/example/ridemetricsdksample/MainActivity.java
+++ app/src/main/java/com/example/ridemetricsdksample/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.ridemetricsdksample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ridemetric.sdk.RideMetric;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(String.format(Locale.getDefault(), "%s-SDK v.%s", getString(R.string.sample_app_name), RideMetric.getVersion()));
     }
 }
